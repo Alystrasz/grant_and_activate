55d3a9126423bb33342e1156883077950e3222eb
Rémy Raes <contact@remyraes.com>
2021-10-12 13:33:45 +0200

    [feat] adding tiles to select features

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,23 +28,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool bluetoothFeature = false;
+  bool locationFeature = false;
+
+  void _switchBluetoothFeatureState(bool? nValue) {
+    setState(() {
+      bluetoothFeature = nValue!;
+    });
+  }
+
+  void _switchLocationFeatureState(bool? nValue) {
+    setState(() {
+      locationFeature = nValue!;
+    });
+  }
+
+  bool _isAtLeastOneFeatureSelected() {
+    return bluetoothFeature || locationFeature;
+  }
+
+  void _grantAndActivateFeatures() {
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Grant and activate example app'),
+        title: const Text('Grant and activate example app'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text(
+                'Select features you want to activate:',
+              ),
+            ),
+            ListTile(
+              title: const Text('Bluetooth'),
+              onTap: () => _switchBluetoothFeatureState(!bluetoothFeature),
+              trailing: Checkbox(value: bluetoothFeature, onChanged: _switchBluetoothFeatureState),
+            ),
+            ListTile(
+              title: const Text('Location'),
+              onTap: () => _switchLocationFeatureState(!locationFeature),
+              trailing: Checkbox(value: locationFeature, onChanged: _switchLocationFeatureState),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                  child: const Text("Grant and activate selected features"),
+                  onPressed: _isAtLeastOneFeatureSelected() ? _grantAndActivateFeatures : null
+              ),
             )
           ],
         ),
-      ) // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
